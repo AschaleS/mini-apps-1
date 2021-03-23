3a90dec019c378adc4eb3a18162405c3557291eb
Aschale <aschaled@gmail.com>
2021-03-23 09:37:32 -0500

    Addedd a function to check who wins the game

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -7,25 +7,43 @@ document.addEventListener('DOMContentLoaded', function (event) {
     boxes[i].addEventListener('click', playGame);
   }
   let turnCounter = 0;
-  let letterX = [];
-  let letterO = [];
+  let arrayOfX = [];
+  let arrayOfO = [];
+  let winningCombinations = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"],   ["1", "5", "9"], ["3", "5", "7"]];
 
 
   function playGame(event) {
     if (turnCounter % 2 === 0) {
-      letterX.push(event.target.id);
+      arrayOfX.push(event.target.id);
       event.target.value = "X";
       event.target.disabled = true;
       turnCounter++;
+      checkWhoWins(arrayOfX, "X");
 
     } else {
-      letterO.push(event.target.id);
+      arrayOfO.push(event.target.id);
       event.target.value = "O";
       event.target.disabled = true;
       turnCounter++;
+      checkWhoWins(arrayOfO, "O");
 
     }
 
   }
+  const checkWhoWins = (arrayOfLetters, player) => {
+    for(let i = 0; i < winningCombinations.length; i++) {
+      let sameLetters = 0;
+      for(var j = 0; j < winningCombinations[i].length; j++) {
+        if(arrayOfLetters.indexOf(winningCombinations[i][j]) !== -1) {
+          sameLetters++;
+        }
+
+        if(sameLetters === 3) {
+          alert("Player: " + player + "wins!");
+        }
+      }
+    }
+  }
+
 
 })
