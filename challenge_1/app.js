e3fda1aad34a3bac5cc9ce30581e24cfad65f1c7
Aschale <aschaled@gmail.com>
2021-03-23 01:17:35 -0500

    Implemented placing alternate X and O values into the boxes

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,5 +1,31 @@
 
 
 document.addEventListener('DOMContentLoaded', function (event) {
-  var boxes = document.getElementsByClassName('box');
+  let boxes = document.getElementsByClassName('box');
+
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', playGame);
+  }
+  let turnCounter = 0;
+  let letterX = [];
+  let letterO = [];
+
+
+  function playGame(event) {
+    if (turnCounter % 2 === 0) {
+      letterX.push(event.target.id);
+      event.target.value = "X";
+      event.target.disabled = true;
+      turnCounter++;
+
+    } else {
+      letterO.push(event.target.id);
+      event.target.value = "O";
+      event.target.disabled = true;
+      turnCounter++;
+
+    }
+
+  }
+
 })
