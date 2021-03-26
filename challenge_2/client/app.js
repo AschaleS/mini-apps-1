2b5b78680ca7aa5a6f415f796d6af8bb8a49ed72
Aschale <aschaled@gmail.com>
2021-03-25 23:57:01 -0500

    Progress Submitting Data via AJAXX

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,14 +1,29 @@
 
-var xhttp = new XMLHttpRequest();
+//var xhttp = new XMLHttpRequest();
 
-const url = 'http://localhost:3000'
+const url = 'http://localhost:3000';
 
 
-// function sendJson(){
-//     xhttp.open("POST", url, true);
-//     console.log("Form button has been clicked for item entry submission");
-//     console.log(xhttp);
-//     var form = new FormData()
-//     form.append("text",document.getElementsByName("text").value)
-//     xhttp.send(form);
-// }
+$(document).ready(function () {
+  //$("form").submit(function (event) {
+    S(.submit).click(function(event) {
+      event.preventDefault();
+    var formData = {
+      text: $("#data").val();
+    };
+
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:3000/",
+      data: formData,
+      dataType: "application/JSON",
+      encode: true,
+    }).done(function (data) {
+      console.log(data);
+    });
+
+    // event.preventDefault();
+  });
+});
+
+
